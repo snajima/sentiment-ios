8fd2e59c592b4db0af6b66067f1373d2c1f6b2d6
Shungo Najima <sn685@cornell.edu>
2022-11-30 14:19:56 -0500

    Initial Commit

---
+++ Sentiment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sentiment
+//
+//  Created by Shungo Najima on 11/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
